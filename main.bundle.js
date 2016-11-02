1c5b9410e88878428d20018462d1009ad5aeb90d
DanGrund <hello@dangrund.com>
2016-11-02 14:50:47 -0600

    reformat file structure

--- main.bundle.js
+++ main.bundle.js
@@ -42,15 +42,98 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var canvas = document.getElementById('game');
+	var context = canvas.getContext('2d');
+	var Ship = __webpack_require__(1);
+
+	var ship = new Ship(300, 300, 30, 30);
+
+	// function Asteroid (x, y, width, height) {
+	//   this.x = x;
+	//   this.y = y;
+	//   this.width = width;
+	//   this.height = height;
+	// }
+	//
+	// var asteroidArray = [];
+
+
+	//asteroid instatiation
+
+	//asteroid/ship collision spect
+
+	requestAnimationFrame(function gameLoop() {
+	  context.clearRect(0, 0, canvas.width, canvas.height);
+	  ship.draw().move();
+	  requestAnimationFrame(gameLoop);
+	});
+
+/***/ },
+/* 1 */
 /***/ function(module, exports) {
 
-	'use strict';
+	function Ship(x, y, width, height) {
+	  this.x = x;
+	  this.y = y;
+	  this.width = width;
+	  this.height = height;
+	}
 
-	var sayHello = function sayHello() {
-	  return console.log('Hello');
+	Ship.prototype.draw = function () {
+	  context.fillRect(this.x, this.y, this.width, this.height);
+	  return this;
 	};
 
-	sayHello();
+	Ship.prototype.move = function () {
+	  if (rightPressed) {
+	    this.x = this.x + 5;
+	  }
+	  if (leftPressed) {
+	    this.x = this.x - 5;
+	  }
+	  if (upPressed) {
+	    this.y = this.y - 5;
+	  }
+	  if (downPressed) {
+	    this.y = this.y + 5;
+	  }
+	};
+
+	var leftPressed = false;
+	var rightPressed = false;
+	var upPressed = false;
+	var downPressed = false;
+
+	document.addEventListener("keydown", keyDownHandler, false);
+	document.addEventListener("keyup", keyUpHandler, false);
+
+	function keyDownHandler(e) {
+	  if (e.keyCode === 39) {
+	    rightPressed = true;
+	  } else if (e.keyCode === 37) {
+	    leftPressed = true;
+	  } else if (e.keyCode === 38) {
+	    upPressed = true;
+	  } else if (e.keyCode === 40) {
+	    downPressed = true;
+	  }
+	}
+
+	function keyUpHandler(e) {
+	  if (e.keyCode === 39) {
+	    rightPressed = false;
+	  } else if (e.keyCode === 37) {
+	    leftPressed = false;
+	  } else if (e.keyCode === 38) {
+	    upPressed = false;
+	  } else if (e.keyCode === 40) {
+	    downPressed = false;
+	  }
+	}
+
+	module.exports = Ship;
 
 /***/ }
 /******/ ]);
